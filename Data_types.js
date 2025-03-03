1d595fe10b3ab9547730b65ea3d133ab3e67841e
Rhoda <rhodachela@gmail.com>
2025-03-03 23:58:30 +0300

    Javascript assignment

---
+++ Data_types.js
@@ -0,0 +1,17 @@
+// Declare variables and assign values
+let firstName = "John";
+let lastName = "Doe";
+let country = "USA";
+let city = "New York";
+let age = 25;
+let isMarried = false;
+let year = 2024;
+
+// Check different data types
+console.log(typeof firstName); // string
+console.log(typeof lastName); // string
+console.log(typeof country); // string
+console.log(typeof city); // string
+console.log(typeof age); // number
+console.log(typeof isMarried); // boolean
+console.log(typeof year); // number
